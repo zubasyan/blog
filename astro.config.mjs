c65a1b0bf30aa188af975875ddb4a4e7c9c06112
zubasyan <mikoraio914@icloud.com>
2026-01-12 16:03:17 +0900

    add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({  
-  site: 'https://zubasyan.github.io',  
+export default defineConfig({
+  site: 'https://zubasyan.github.io',
   base: '/site',
+  integrations: [preact()],
 });
