b9924084e25439d7484f07a3849a0bf884246269
zubasyan <mikoraio914@icloud.com>
2026-01-30 22:27:17 +0900

    update erasability.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,13 @@ import rehypeKatex from 'rehype-katex';
 
 import preact from '@astrojs/preact';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zubasyan.github.io',
   base: '/site',
-  integrations: [preact()],
+  integrations: [preact(), mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
